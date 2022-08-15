48061bf78c535c3bdfa4b2fe220a617ca33e6f48
karol50709 <karol50709@gmail.com>
2022-08-16 00:03:01 +0200

    merge test

---
+++ master/src/main/java/com/karol/supervisorserverandworkers/master/service/JobQueueService.java
@@ -0,0 +1,9 @@
+package com.karol.supervisorserverandworkers.master.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class JobQueueService {
+}
